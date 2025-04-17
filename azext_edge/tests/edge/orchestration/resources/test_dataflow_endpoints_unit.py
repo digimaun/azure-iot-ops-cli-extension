3f6b0d98097109027336f1bd127cbb19fefe6472
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-04-16 18:03:48 -0700

    WIP

--- azext_edge/tests/edge/orchestration/resources/test_dataflow_endpoints_unit.py
+++ azext_edge/tests/edge/orchestration/resources/test_dataflow_endpoints_unit.py
@@ -38,6 +38,7 @@ def get_mock_dataflow_endpoint_record(
             "provisioningState": "Succeeded",
         },
         resource_group_name=resource_group_name,
+        qualified_type="microsoft.iotoperations/instances/dataflowendpoints",
     )
 
 
